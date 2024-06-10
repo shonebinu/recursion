4c926e9d2b02de7279beaca425a3ee898bb9c0ec
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2024-06-10 11:14:18 +0530

    implement recursive fibonacci series

--- fibonacci.js
+++ fibonacci.js
@@ -9,3 +9,13 @@ function fibs(n) {
 
 	return sequence;
 }
+
+function fibsRec(n) {
+	if (n <= 0) return [];
+	if (n === 1) return [0];
+	if (n === 2) return [0, 1];
+
+	const sequence = fibsRec(n - 1);
+
+	return [...sequence, sequence.at(-1) + sequence.at(-2)];
+}
